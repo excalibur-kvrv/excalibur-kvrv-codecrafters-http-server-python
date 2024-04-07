5e39d62614ae41854434a504f5bc79cca780ee63
excalibur-kvrv <excalibur.krv@gmail.com>
2024-04-07 19:46:32 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,14 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    con, addr = server_socket.accept() # wait for client
+    with con:
+        data = con.recv(4096)
+        if data.endswith(b"\r\n\r\n"):
+            con.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            print("Sent data")
+        print(data, data.endswith(b"\r\n\r\n"))
+
 
 
 if __name__ == "__main__":
