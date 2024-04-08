5f3eb9009735533719dafdb52e5711ccd8dc284e
excalibur-kvrv <excalibur.krv@gmail.com>
2024-04-08 23:17:45 +0530

    completed stage 5

--- app/main.py
+++ app/main.py
@@ -17,13 +17,17 @@ def main():
             parsed_request = data.decode("utf-8")
             parsed_request_lines = parsed_request.split(ENDLINE)
             http_method, path, http_version = parsed_request_lines[0].split(" ")
-            print(http_method, path, http_version)
+            request_headers = parsed_request_lines[1:]
             if path == "/":
                 con.sendall(b"HTTP/1.1 200 OK" + (ENDLINE * 2).encode())
             elif path.startswith("/echo"):
                 random_string = path.replace("/echo/", "")
-                print(random_string.encode())
-                con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + bytes(str(len(random_string)), "utf-8") + (ENDLINE * 2).encode() + random_string.encode()) # + (ENDLINE * 4).encode())
+                con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + bytes(str(len(random_string)), "utf-8") + (ENDLINE * 2).encode() + random_string.encode()) 
+            elif path.startswith("/user-agent"):
+                for header in request_headers:
+                    if header.startswith("User-Agent:"):
+                        user_agent_body = header.replace("User-Agent: ", "")
+                        con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + bytes(str(len(user_agent_body)), "utf-8") + (ENDLINE * 2).encode() + user_agent_body.encode()) 
             else:
                 con.sendall(b"HTTP/1.1 404 Not Found" + (ENDLINE * 2).encode())
 
