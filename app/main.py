5bab96fdac734015c3e204222bda0e98770edeb8
excalibur-kvrv <excalibur.krv@gmail.com>
2024-04-08 23:06:51 +0530

    completed stage 4

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # Uncomment this to pass the first stage
 import socket
 
+ENDLINE = "\r\n"
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,16 +13,19 @@ def main():
     con, addr = server_socket.accept() # wait for client
     with con:
         data = con.recv(4096)
-        if data.endswith(b"\r\n\r\n"):
+        if data.endswith((ENDLINE * 2).encode()):
             parsed_request = data.decode("utf-8")
-            parsed_request_lines = parsed_request.split("\r\n")
+            parsed_request_lines = parsed_request.split(ENDLINE)
             http_method, path, http_version = parsed_request_lines[0].split(" ")
             print(http_method, path, http_version)
             if path == "/":
-                con.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+                con.sendall(b"HTTP/1.1 200 OK" + (ENDLINE * 2).encode())
+            elif path.startswith("/echo"):
+                random_string = path.replace("/echo/", "")
+                print(random_string.encode())
+                con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + bytes(str(len(random_string)), "utf-8") + (ENDLINE * 2).encode() + random_string.encode()) # + (ENDLINE * 4).encode())
             else:
-                con.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-        print(data, data.endswith(b"\r\n\r\n"))
+                con.sendall(b"HTTP/1.1 404 Not Found" + (ENDLINE * 2).encode())
 
 
 
