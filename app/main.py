390eef3359a3aa2aab655254bef1b60cc2d038a6
excalibur-kvrv <excalibur.krv@gmail.com>
2024-04-09 01:23:50 +0530

    completed stage 8

--- app/main.py
+++ app/main.py
@@ -5,37 +5,53 @@ import socket
 
 ENDLINE = "\r\n"
 
+def process_get(con, path, file_path, request_headers):
+    match path:
+        case "/":
+            con.sendall(b"HTTP/1.1 200 OK" + (ENDLINE * 2).encode())
+        case s if s.startswith("/echo"):
+            random_string = path.replace("/echo/", "")
+            con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + bytes(str(len(random_string)), "utf-8") + (ENDLINE * 2).encode() + random_string.encode()) 
+        case s if s.startswith("/user-agent"):
+            for header in request_headers:
+                if header.startswith("User-Agent:"):
+                    user_agent_body = header.replace("User-Agent: ", "")
+                    con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + bytes(str(len(user_agent_body)), "utf-8") + (ENDLINE * 2).encode() + user_agent_body.encode()) 
+        case s if s.startswith("/files"):
+            file_name = path.replace("/files/", "")
+            if os.path.exists(os.path.join(file_path, file_name)):
+                with open(os.path.join(file_path, file_name), "rb") as file:
+                    contents = file.read()
+                    con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + bytes(str(len(contents)), "utf-8") + (ENDLINE * 2).encode() + contents)
+            else:
+                con.sendall(b"HTTP/1.1 404 Not Found" + (ENDLINE * 2).encode())
+        case _:
+            con.sendall(b"HTTP/1.1 404 Not Found" + (ENDLINE * 2).encode())
+
+
+def process_post(con, path, file_path, request_headers):
+    match path:
+        case s if s.startswith("/files"):
+            file_name = path.replace("/files/", "")
+            if not os.path.exists(os.path.join(file_path, file_name)):
+                with open(os.path.join(file_path, file_name), "wb") as file:
+                    file.write(request_headers[-1].encode())
+                con.sendall(b"HTTP/1.1 201 OK\r\n")
+            else:
+                con.sendall(b"HTTP/1.1 400 Bad Request" + (ENDLINE * 2).encode())
+    print(request_headers)
 def server_connection(con, file_path):
     print("Recieved Connection From", con)
     with con:
         data = con.recv(4096)
-        if data.endswith((ENDLINE * 2).encode()):
-            parsed_request = data.decode("utf-8")
-            parsed_request_lines = parsed_request.split(ENDLINE)
-            http_method, path, http_version = parsed_request_lines[0].split(" ")
-            request_headers = parsed_request_lines[1:]
-            match path:
-                case "/":
-                    con.sendall(b"HTTP/1.1 200 OK" + (ENDLINE * 2).encode())
-                case s if s.startswith("/echo"):
-                    random_string = path.replace("/echo/", "")
-                    con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + bytes(str(len(random_string)), "utf-8") + (ENDLINE * 2).encode() + random_string.encode()) 
-                case s if s.startswith("/user-agent"):
-                    for header in request_headers:
-                        if header.startswith("User-Agent:"):
-                            user_agent_body = header.replace("User-Agent: ", "")
-                            con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + bytes(str(len(user_agent_body)), "utf-8") + (ENDLINE * 2).encode() + user_agent_body.encode()) 
-                case s if s.startswith("/files"):
-                    file_name = path.replace("/files/", "")
-                    if os.path.exists(os.path.join(file_path, file_name)):
-                        with open(os.path.join(file_path, file_name), "rb") as file:
-                            contents = file.read()
-                            con.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + bytes(str(len(contents)), "utf-8") + (ENDLINE * 2).encode() + contents)
-                    else:
-                        con.sendall(b"HTTP/1.1 404 Not Found" + (ENDLINE * 2).encode())
-                case _:
-                    con.sendall(b"HTTP/1.1 404 Not Found" + (ENDLINE * 2).encode())
-
+        parsed_request = data.decode("utf-8")
+        parsed_request_lines = parsed_request.split(ENDLINE)
+        http_method, path, http_version = parsed_request_lines[0].split(" ")
+        request_headers = parsed_request_lines[1:]
+        if http_method == "GET":
+            process_get(con, path, file_path, request_headers)
+        elif http_method == "POST":
+            process_post(con, path, file_path, request_headers)
 
 
 def main():
